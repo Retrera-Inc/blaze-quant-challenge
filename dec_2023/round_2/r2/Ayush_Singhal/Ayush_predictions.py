b4cc99643b65b2f9261c597fb2119a5ff4fb6e7b
Ayush Singhal <91676003+AyushSinghal9020@users.noreply.github.com>
2024-01-03 09:20:56 +0530

    Update Ayush_predictions.py

--- dec_2023/round_2/r2/Ayush_Singhal/Ayush_predictions.py
+++ dec_2023/round_2/r2/Ayush_Singhal/Ayush_predictions.py
@@ -1,117 +1,169 @@
 # predictions_<team_lead_name>.py
+from instructions import ETHPriceRanges
 
 from enum import Enum
-import numpy as np
-import pandas as pd
+import pandas as pd 
+import numpy as np 
 import requests
-from sklearn.model_selection import train_test_split
+
 from sklearn.preprocessing import StandardScaler
-from keras.models import Sequential
-from keras.layers import LSTM, Dense
-from keras.callbacks import ModelCheckpoint, EarlyStopping
-from sklearn.metrics.pairwise import cosine_similarity
-# from instructions import ETHPriceRanges
 
+import torch 
+from torch.utils.data import Dataset
+from torch.utils.data import DataLoader
+from pytorch_lightning import LightningDataModule as LDM
+from pytorch_lightning import LightningModule as LM
+from pytorch_lightning.callbacks import ModelCheckpoint, EarlyStopping
+from pytorch_lightning import Trainer
 
-class ETHPriceRanges(Enum):
-    pr_2000_2025 = 1
-    pr_2025_2050 = 2
-    pr_2050_2075 = 3
-    pr_2075_2100 = 4
-    pr_2100_2125 = 5
-    pr_2125_2150 = 6
-    pr_2150_2175 = 7
-    pr_2175_2200 = 8
-    pr_2200_2225 = 9
-    pr_2225_2250 = 10
-    pr_2250_2275 = 11
-    pr_2275_2300 = 12
-    pr_2300_2325 = 13
-    pr_2325_2350 = 14
-    pr_2350_2375 = 15
-    pr_2375_2400 = 16
-    pr_2400_2425 = 17
-    pr_2425_2450 = 18
-    pr_2450_2475 = 19
-    pr_2475_2500 = 20
-    pr_2500_2525 = 21
-    pr_2525_2550 = 22
-    pr_2550_2575 = 23
-    pr_2575_2600 = 24
+import torch.nn as nn
 
+class data(Dataset) : 
 
-def get_historical_data(fsym , tsym , limit , aggregate , toTs = None , data_list = []):
+    def __init__(self) : 
 
-    url = f'https://min-api.cryptocompare.com/data/v2/histohour?fsym={fsym}&tsym={tsym}&limit={limit}&aggregate={aggregate}'
+        super().__init__()
+            
+        self.X = np.load('Inputs.npy' , allow_pickle = True)
+        self.y = np.load('Labels.npy' , allow_pickle = True)
+        
+    def __len__(self) : return self.X.shape[0]
 
-    if toTs : url += f'&toTs={toTs}'
-    try :
-        value = requests.get(url).json()['Data']
+    def __getitem__(self , index) : 
 
-        val = value['Data']
+        r_embed = torch.tensor(self.X[index] , dtype = torch.float32)
+        r_target = torch.tensor(self.y[index] , dtype = torch.float32)
 
-        data_list.append(val)
+        return r_embed , r_target
 
-        if value['TimeFrom'] <= 1438214400 : return data_list
-        else : return get_historical_data(fsym , tsym , limit , aggregate , toTs = value['TimeFrom'] , data_list = data_list)
+class Data(LDM) : 
 
-    except Exception as e : return data_list
+    def __init__(self , batch_size = 32) : 
 
-def create_sequences(data, sequence_length):
-    sequences, labels = [], []
-    for i in range(len(data) - sequence_length):
-        sequence = data[i : i + sequence_length]
-        label = data[i + sequence_length, -1]  # Closing price as the label
-        sequences.append(sequence)
-        labels.append(label)
-    return np.array(sequences), np.array(labels)
+        super().__init__()
 
+        self.batch_size = batch_size
 
-def train_lstm_model(data):
-    model = Sequential()
-    model.add(LSTM(units=50, input_shape=(data.shape[1], data.shape[2])))
-    model.add(Dense(units=1))
+    def setup(self , stage = None) : self.train = data()
 
-    return model
+    def train_dataloader(self) : return DataLoader(self.train , batch_size = self.batch_size)
 
+data_module = Data()
+
+class lightning(LM) : 
+
+    def __init__(self) : 
+
+        super().__init__()
+
+        self.lstm = nn.LSTM(input_size = 3 , hidden_size = 10 , num_layers = 50) 
+        self.line = nn.Linear(10 , 1)
+        self.loss_func = nn.MSELoss()
+
+    def forward(self , inp) : 
+
+        emb = self.lstm(inp)[1][0][0]
+        emb = self.line(emb)
+
+        return emb
+
+    def training_step(self , batch , batch_idx) : 
+
+        inputs , labels = batch
+        outputs = self(inputs)
+
+        loss = self.loss_func(outputs , labels)
+        self.log('train_loss' , loss)
+
+        return loss
+
+    def configure_optimizers(self) : return torch.optim.Adam(self.parameters())
+
+checkpoint_callback = ModelCheckpoint(
+    monitor='train_loss',
+    filename='best_model',
+    save_top_k=1,
+    mode='min',
+    save_weights_only=True,
+)
+early_stopping_callback = EarlyStopping(
+    monitor='train_loss',
+    patience=10,
+    mode='min',
+)
+
+model = lightning()
+
+trainer = Trainer(
+    max_epochs=1, 
+    callbacks=[checkpoint_callback, early_stopping_callback])
+
+trainer.fit(model , data_module)
 
 def predictions():
-    historical_data = get_historical_data('ETH', 'USD', 2000, 1)
-    data = pd.json_normalize([item for sublist in historical_data for item in sublist])
-    data.drop(['conversionType' , 'conversionSymbol']  , axis = 1 , inplace = True)
-    data = data[::-1].reset_index(drop=True)
-    data_selected = data[['high', 'low', 'open']].values
+    data = requests.get('https://min-api.cryptocompare.com/data/v2/histohour?fsym=ETH&tsym=USD&limit=10&aggregate=1').json()['Data']['Data']
+    data = pd.json_normalize(data)
+    data = data[['high', 'low', 'open']]
+    X_input = data.values
+    
     scaler = StandardScaler()
-    data_scaled = scaler.fit_transform(data_selected)
-    sequence_length = 10
-    X, y = create_sequences(data_scaled, sequence_length)
-    X_train, X_temp, y_train, y_temp = train_test_split(X, y, test_size=0.2, random_state=42)
-    X_val, X_test, y_val, y_test = train_test_split(X_temp, y_temp, test_size=0.5, random_state=42)
-    model = train_lstm_model(X_train)
-    model.compile(optimizer='adam', loss='mean_squared_error')
-    checkpoint = ModelCheckpoint('best_model.h5', save_best_only=True)
-    early_stopping = EarlyStopping(monitor='val_loss', patience=10)
-    history = model.fit(X_train, y_train, epochs=5, batch_size=32, validation_data=(X_val, y_val),
-                        callbacks=[checkpoint, early_stopping])
-    test_loss = model.evaluate(X_test, y_test)
-    y_pred = model.predict(X_test)
-    y_pred_original = scaler.inverse_transform(np.hstack((X_test[:, -1, :-1], y_pred.reshape(-1, 1))))
-    y_test_original = scaler.inverse_transform(np.hstack((X_test[:, -1, :-1], y_test.reshape(-1, 1))))
-    threshold = 0.02
-    cosine_sim = cosine_similarity(y_test.reshape(1, -1), y_pred.reshape(1, -1))[0, 0]
+    X_input_scaled = scaler.fit_transform(X_input)
+    
+    X_input_tensor = torch.tensor(X_input_scaled, dtype=torch.float32)
+    
+    model.eval()
+    
+    num_steps = 7
+    predictions = []
+    prices = []
+    
+    for index in range(num_steps):
+    
+        data_to_inverse_transform = np.vstack((X_input_tensor[-9:].numpy(), np.repeat(model(X_input_tensor[-10:]).detach().numpy(), 3, axis=0)))
+        y_pred_single_original = scaler.inverse_transform(data_to_inverse_transform)
+    
+        predictions.append(y_pred_single_original[index])
+    
+        X_input = np.vstack((X_input, y_pred_single_original))
+    
+        X_input = X_input[-10:]
+    
+        prices.append(y_pred_single_original[index, 2])
 
-    return [
-        ETHPriceRanges.pr_2300_2325,
-        ETHPriceRanges.pr_2300_2325,
-        ETHPriceRanges.pr_2300_2325,
-        ETHPriceRanges.pr_2300_2325,
-        ETHPriceRanges.pr_2300_2325,
-        ETHPriceRanges.pr_2300_2325,
-        ETHPriceRanges.pr_2300_2325,
-    ]
+    return_val = []
+
+    for val in prices : 
+        if val >= 2000 and val < 2025 : return_val.append(ETHPriceRanges.pr_2000_2025)
+        elif val >= 2025 and val < 2050 : return_val.append(ETHPriceRanges.pr_2025_2050)
+        elif val >= 2050 and val < 2075 : return_val.append(ETHPriceRanges.pr_2050_2075)
+        elif val >= 2075 and val < 2100 : return_val.append(ETHPriceRanges.pr_2075_2100)
+        elif val >= 2100 and val < 2125 : return_val.append(ETHPriceRanges.pr_2100_2125)
+        elif val >= 2125 and val < 2150 : return_val.append(ETHPriceRanges.pr_2125_2150)
+        elif val >= 2150 and val < 2175 : return_val.append(ETHPriceRanges.pr_2150_2175)
+        elif val >= 2175 and val < 2200 : return_val.append(ETHPriceRanges.pr_2175_2200)
+        elif val >= 2200 and val < 2225 : return_val.append(ETHPriceRanges.pr_2200_2225)
+        elif val >= 2225 and val < 2250 : return_val.append(ETHPriceRanges.pr_2225_2250)
+        elif val >= 2250 and val < 2275 : return_val.append(ETHPriceRanges.pr_2250_2275)
+        elif val >= 2275 and val < 2300 : return_val.append(ETHPriceRanges.pr_2275_2300)
+        elif val >= 2300 and val < 2325 : return_val.append(ETHPriceRanges.pr_2300_2325)
+        elif val >= 2325 and val < 2350 : return_val.append(ETHPriceRanges.pr_2325_2350)
+        elif val >= 2350 and val < 2375 : return_val.append(ETHPriceRanges.pr_2350_2375)
+        elif val >= 2375 and val < 2400 : return_val.append(ETHPriceRanges.pr_2375_2400)
+        elif val >= 2400 and val < 2425 : return_val.append(ETHPriceRanges.pr_2400_2425)
+        elif val >= 2425 and val < 2450 : return_val.append(ETHPriceRanges.pr_2425_2450)
+        elif val >= 2450 and val < 2475 : return_val.append(ETHPriceRanges.pr_2450_2475)
+        elif val >= 2475 and val < 2500 : return_val.append(ETHPriceRanges.pr_2475_2500)
+        elif val >= 2500 and val < 2525 : return_val.append(ETHPriceRanges.pr_2500_2525)
+        elif val >= 2525 and val < 2550 : return_val.append(ETHPriceRanges.pr_2525_2550)
+        elif val >= 2550 and val < 2575 : return_val.append(ETHPriceRanges.pr_2550_2575)
+        elif val >= 2575 and val < 2600 : return_val.append(ETHPriceRanges.pr_2575_2600)
+        else : return_val.append(ETHPriceRanges.pr_2600_2625)
+    
+    return return_val
 
 
 # DO NOT REMOVE
 preds = predictions()
+print(preds)
 assert len(preds) == 7
 assert all([isinstance(val, ETHPriceRanges) for val in preds])

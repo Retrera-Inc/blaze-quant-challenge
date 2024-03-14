d7424e2790b489b7570f9d2d307ac4ad87df1564
Chirag Mahapatra <chiragmahapatra@Chirags-MacBook-Pro.local>
2024-03-14 11:18:43 -0700

    fix instructions for march predictions

--- mar_2024/round_2/example.py
+++ mar_2024/round_2/example.py
@@ -3,10 +3,10 @@ Copy this file and add your business logic for the predictions. Add your file in
 the format: predictions_<team_lead_name>.py
 """
 
-from instructions import ETHPriceRanges
+from instructions import ETHPriceRanges, ARBPriceRanges, LINKPriceRanges
 
 
-def predictions():
+def predictions_ETH():
     """
     All of the business logic should go here. The output should be a list
     of size 7 with values from ETHPriceRanges
@@ -22,9 +22,49 @@ def predictions():
     ]
 
 
+def predictions_ARB():
+    """
+    All of the business logic should go here. The output should be a list
+    of size 7 with values from ARBPriceRanges
+    """
+    return [
+        ARBPriceRanges.pr_180_185,
+        ARBPriceRanges.pr_180_185,
+        ARBPriceRanges.pr_180_185,
+        ARBPriceRanges.pr_180_185,
+        ARBPriceRanges.pr_180_185,
+        ARBPriceRanges.pr_180_185,
+        ARBPriceRanges.pr_180_185,
+    ]
+
+
+def predictions_LINK():
+    """
+    All of the business logic should go here. The output should be a list
+    of size 7 with values from LINKPriceRanges
+    """
+    return [
+        LINKPriceRanges.pr_1875_1900,
+        LINKPriceRanges.pr_1875_1900,
+        LINKPriceRanges.pr_1875_1900,
+        LINKPriceRanges.pr_1875_1900,
+        LINKPriceRanges.pr_1875_1900,
+        LINKPriceRanges.pr_1875_1900,
+        LINKPriceRanges.pr_1875_1900,
+    ]
+
+
 """
 DO NOT REMOVE
 """
-preds = predictions()
-assert len(preds) == 7
-assert all([isinstance(val, ETHPriceRanges) for val in preds])
+preds_ETH = predictions_ETH()
+assert len(preds_ETH) == 7
+assert all([isinstance(val, ETHPriceRanges) for val in preds_ETH])
+
+preds_ARB = predictions_ARB()
+assert len(preds_ARB) == 7
+assert all([isinstance(val, ARBPriceRanges) for val in preds_ARB])
+
+preds_LINK = predictions_LINK()
+assert len(preds_LINK) == 7
+assert all([isinstance(val, LINKPriceRanges) for val in preds_LINK])

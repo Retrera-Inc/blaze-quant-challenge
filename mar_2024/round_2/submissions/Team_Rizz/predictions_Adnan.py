6b28331198f0ad8d8dd4a6531e0900bc0fa1c0a9
prakharmoses <me21b054@iittp.ac.in>
2024-03-31 00:40:08 +0530

    Recently updated code for prediction from 31st

--- mar_2024/round_2/submissions/Team_Rizz/predictions_Adnan.py
+++ mar_2024/round_2/submissions/Team_Rizz/predictions_Adnan.py
@@ -30,10 +30,10 @@ def str_to_datetime(str):
     return datetime.datetime(year=year,month=month,day=day)
 
 def df_to_windowed_df(dataframe, first_date_str, last_date_str, n=3):
-#   print(first_date)
+#   # print(first_date)
   first_date = str_to_datetime(first_date_str)
   last_date  = str_to_datetime(last_date_str)
-#   print(first_date)
+#   # print(first_date)
 
   target_date = first_date
 #   target_date = np.datetime64(target_date)
@@ -45,7 +45,7 @@ def df_to_windowed_df(dataframe, first_date_str, last_date_str, n=3):
     df_subset = dataframe.loc[:target_date].tail(n+1)
     
     if len(df_subset) != n+1:
-      print(f'Error: Window of size {n} is too large for date {target_date}')
+      # print(f'Error: Window of size {n} is too large for date {target_date}')
       return
 
     values = df_subset['Price'].to_numpy()
@@ -166,44 +166,44 @@ def predictions_ETH():
     # today_date = us_eastern.localize(today_datetime)
     us_eastern = pytz.timezone('US/Eastern')
     today_date = datetime.datetime(today_date.year, today_date.month, today_date.day, tzinfo=pytz.utc).astimezone(us_eastern).date()
-    print(today_date)
+    # # print(today_date)
     # today_date = datetime.date(2024, 3, 16)
     today_data = yf.download('ETH-USD', start='2024-03-15', end=today_date)
     today_data.drop(columns=['Open', 'High', 'Low', 'Adj Close', 'Volume'], inplace=True)
     today_data.rename(columns={"Close":"Price"}, inplace=True)
-    # print("The data without the modification is: \n", today_data)
+    # # print("The data without the modification is: \n", today_data)
     if (len(array) > 0):
        addional_date = today_date + datetime.timedelta(days=len(array)-1)
        additional_data = pd.DataFrame({"Price": array[-1]}, index=[pd.to_datetime(addional_date)])
        today_data = pd.concat([today_data, additional_data])
-    if (len(array) == 0): print("The data printed to get the desired array is: \n", today_data)
+    # if (len(array) == 0): # print("The data printed to get the desired array is: \n", today_data)
     
 
     # Load the Ethereum data and prearing it
     df=pd.read_csv('./ethereumData.csv')
     # df['Price']=pd.to_numeric(df['Price'])
     df=df.iloc[::-1]
-    # print(today_data.index.dtype)
+    # # print(today_data.index.dtype)
     df["Date"]=df["Date"].astype(str)
     df["Date"] = df['Date'].apply(str_to_datetime)
     # df.index=df.pop('Date')
-    # print(df.head())
+    # # print(df.head())
     df1=df.drop(columns=["Volume","Market_cap"])
-    # print(df1)
+    # # print(df1)
     df3 = df1
     df3.index=df3.pop('Date')
-    # print(df3)
+    # # print(df3)
 
     df4 = pd.concat([df3, today_data])      # Concatenating today's data with the historical data
-    # print("The data is\n", df4)
+    # # print("The data is\n", df4)
 
     # Start day second time around: '2021-03-25'
     # today = datetime.date.today() - datetime.timedelta(days = 2)
     today = today_date - datetime.timedelta(days = 1)
     # today = today_date - datetime.timedelta(days = 1)
     today = today.strftime("%Y-%m-%d")
-    # print(today)
-    # print(type(today))
+    # # print(today)
+    # # print(type(today))
     #yaha pe change hua hai. prakhar please notice.
     windowed_df = df_to_windowed_df(df4,  '2018-08-11', 
                                     today, 
@@ -212,7 +212,7 @@ def predictions_ETH():
     # windowed_df
 
     dates, X, y = windowed_df_to_date_X_y(windowed_df)
-    # print(dates.shape,X.shape," ",y.shape)
+    # # print(dates.shape,X.shape," ",y.shape)
 
     q_80 = int(len(dates) * .89)
     q_90 = int(len(dates) * 1)
@@ -242,11 +242,11 @@ def predictions_ETH():
     model.fit(X_train, y_train, validation_data=(X_val, y_val), epochs=25)
 
     test_predictions = model.predict(X_test).flatten()
-    # print(test_predictions)
+    # # print(test_predictions)
     array.append(test_predictions[0])
 
     # return find_price_range_ETH(test_predictions[0])
-  print(array)
+  # print(array)
   return find_price_range_ETH(array)
 
 
@@ -255,22 +255,31 @@ def predictions_ARB():
     while len(array) < 7:
       # Fetching today's data from yfinance
       today_date = datetime.date.today()
-      # today_data = yf.download('ARB-USD', start='2024-03-15', end=today_date)
-      # today_data.drop(columns=['Open', 'High', 'Low', 'Adj Close', 'Volume'], inplace=True)
-      # today_data.rename(columns={"Close":"Price"}, inplace=True)
-      today_data = pd.DataFrame()
+      # today_datetime = datetime(today_date.year, today_date.month, today_date.day)
+      # today_date = us_eastern.localize(today_datetime)
+      us_eastern = pytz.timezone('US/Eastern')
+      today_date = datetime.datetime(today_date.year, today_date.month, today_date.day, tzinfo=pytz.utc).astimezone(us_eastern).date()
+      
+      today_data = yf.download('ARB11841-USD', start='2024-03-29', end=today_date)
+      today_data.drop(columns=['Open', 'High', 'Low', 'Adj Close', 'Volume'], inplace=True)
+      today_data.rename(columns={"Close":"Price"}, inplace=True)
+      
+      
       if (len(array) > 0):
         addional_date = today_date + datetime.timedelta(days=len(array)-1)
         additional_data = pd.DataFrame({"Price": array[-1]}, index=[pd.to_datetime(addional_date)])
         today_data = pd.concat([today_data, additional_data])
-      # if (len(array) == 0): print("The data printed to get the desired array is: \n", today_data)
-      # print(today_data)
+        
+      # if (len(array) == 0): # print("The data printed to get the desired array is: \n", today_data)
+      # # print(today_data)
 
 
 
       # Load the Ethereum data and prearing it
       df=pd.read_csv('arbitrumData.csv')
       df2=pd.read_csv('dataset.csv')
+      
+
       coin_data=df2[df2['crypto_name']=='Dogecoin' ]
       coin_data.index = pd.Series(np.arange(coin_data["date"].size) + 1)
       coin_data=coin_data.iloc[:1717]
@@ -289,19 +298,19 @@ def predictions_ARB():
       df3= pd.concat([coin_data, df1])
       df3.index=df3.pop('Date')
       df3 = pd.concat([df3, today_data])      # Concatenating today's data with the historical data
-
+      
       # Start day second time around: '2021-03-25'
-      today = datetime.date.today() - datetime.timedelta(days = 1)
+      today = today_date - datetime.timedelta(days = 1)
       today = today.strftime("%Y-%m-%d")
-      # print(today)
-      # print(type(today))
+      # # print(today)
+      # # print(type(today))
       windowed_df = df_to_windowed_df(df3,  '2018-09-28', 
                                       today, 
                                       n=3)
-      # print("Here", windowed_df)
-
+      # # print("Here", windowed_df)
+     
       dates, X, y = windowed_df_to_date_X_y(windowed_df)
-      # print(dates.shape,X.shape," ",y.shape)
+      # # print(dates.shape,X.shape," ",y.shape)
 
       q_80 = int(len(dates) * .89)
       q_90 = int(len(dates) * 1)
@@ -331,10 +340,10 @@ def predictions_ARB():
       model.fit(X_train, y_train, validation_data=(X_val, y_val), epochs=10)
 
       test_predictions = model.predict(X_test).flatten()
-      # print(test_predictions)
+      # # print(test_predictions)
       array.append(test_predictions[0])
       # return find_price_range_ARB(test_predictions[0])
-    print(array)
+    # print(array)
     return find_price_range_ARB(array)
 
 
@@ -345,7 +354,7 @@ def predictions_LINK():
       today_date = datetime.date.today()
       us_eastern = pytz.timezone('US/Eastern')
       today_date = datetime.datetime(today_date.year, today_date.month, today_date.day, tzinfo=pytz.utc).astimezone(us_eastern).date()
-      print(today_date)
+      # print(today_date)
       today_data = yf.download('LINK-USD', start='2024-03-15', end=today_date)
       today_data.drop(columns=['Open', 'High', 'Low', 'Adj Close', 'Volume'], inplace=True)
       today_data.rename(columns={"Close":"Price"}, inplace=True)
@@ -353,41 +362,41 @@ def predictions_LINK():
         addional_date = today_date + datetime.timedelta(days=len(array)-1)
         additional_data = pd.DataFrame({"Price": array[-1]}, index=[pd.to_datetime(addional_date)])
         today_data = pd.concat([today_data, additional_data])
-      if (len(array) == 0): print("The data printed to get the desired array is: \n", today_data)
-      # print(today_data)
+      # if (len(array) == 0): # print("The data printed to get the desired array is: \n", today_data)
+      # # print(today_data)
       
 
       # Load the Ethereum data and prearing it
       df=pd.read_csv('./chainlinkData.csv')
       # df['Price']=pd.to_numeric(df['Price'])
       df=df.iloc[::-1]
-      # print(today_data.index.dtype)
+      # # print(today_data.index.dtype)
       df["Date"]=df["Date"].astype(str)
       df["Date"] = df['Date'].apply(str_to_datetime)
       # df.index=df.pop('Date')
-      # print(df.head())
+      # # print(df.head())
       df1=df.drop(columns=["Volume","Market_cap"])
-      # print(df1)
+      # # print(df1)
       df3 = df1
       df3.index=df3.pop('Date')
-      # print(df3)
+      # # print(df3)
 
       df4 = pd.concat([df3, today_data])      # Concatenating today's data with the historical data
-      # print("The data is\n", df4)
+      # # print("The data is\n", df4)
 
       # Start day second time around: '2021-03-25'
       # today = datetime.date.today() - datetime.timedelta(days = 2)
       today = today_date - datetime.timedelta(days = 1)
       today = today.strftime("%Y-%m-%d")
-      # print(today)
-      # print(type(today))
+      # # print(today)
+      # # print(type(today))
       windowed_df = df_to_windowed_df(df4,  '2017-09-25', 
                                       today, 
                                       n=3)
-      # print("The windowed df is: ", windowed_df)
+      # # print("The windowed df is: ", windowed_df)
 
       dates, X, y = windowed_df_to_date_X_y(windowed_df)
-      # print(dates.shape,X.shape," ",y.shape)
+      # # print(dates.shape,X.shape," ",y.shape)
 
       q_80 = int(len(dates) * .89)
       q_90 = int(len(dates) * 1)
@@ -417,10 +426,10 @@ def predictions_LINK():
       model.fit(X_train, y_train, validation_data=(X_val, y_val), epochs=10)
 
       test_predictions = model.predict(X_test).flatten()
-      # print(test_predictions)
+      # # print(test_predictions)
       array.append(test_predictions[0])
       # return find_price_range_LINK(test_predictions[0])
-    print(array)
+    # print(array)
     return find_price_range_LINK(array)
 
 # print("The price of the Ethereum is = ", predictions_ETH())
